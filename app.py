16c28270082f3940c0d6c13f3d8cf3fcd76e49c6
LuckyLuk546 <Lukas546@hotmail.com>
2021-06-17 20:59:54 +0200

    admin login ses check fix

--- app.py
+++ app.py
@@ -114,155 +114,187 @@ class DeleteCar(FlaskForm):
 
 @app.route('/add_car', methods=['GET', 'POST'])
 def add_car():
-    if session['admin_login'] == 'not_admin':
-        return redirect('/')
-    else:
-        now = datetime.now()
-        form1 = AddCar()
-        if form1.validate_on_submit():
-            car_brand = request.form['car_brand']
-            car_model = request.form['car_model']
-            car_sub_model = request.form['car_sub_model']
-            car_mileage = request.form['car_mileage']
-            car_model_year = request.form['car_model_year']
-            car_horsepower = request.form['car_horsepower']
-            car_sold = request.form['car_sold']
-            car_date_added = now
-            car_price = request.form['car_price']
-            car_thumbnail = request.form['car_thumbnail']
-            car_image_2 = request.form['car_image_2']
-            car_image_3 = request.form['car_image_3']
-            car_image_4 = request.form['car_image_4']
-            car = cars(car_brand, car_model, car_sub_model, car_mileage, car_model_year, car_horsepower, car_sold, car_date_added, car_price, car_thumbnail, car_image_2, car_image_3, car_image_4)
-            db.session.add(car)
-            db.session.commit()
-            message = f"{car_brand} {car_model} {car_sub_model} er tilføjet til databasen."
-            return render_template('add_car.html', message=message)
+    if 'admin_login' in session:
+        if session['admin_login'] == 'not_admin':
+            return redirect('/')
         else:
-            for field, errors in form1.errors.items():
-                for error in errors:
-                    flash("Error in {}: {}".format(
-                        getattr(form1, field).label.text,
-                        error
-                    ), 'error')
-            return render_template('add_car.html', form1=form1, title='Tilføj ny bil')
+            now = datetime.now()
+            form1 = AddCar()
+            if form1.validate_on_submit():
+                car_brand = request.form['car_brand']
+                car_model = request.form['car_model']
+                car_sub_model = request.form['car_sub_model']
+                car_mileage = request.form['car_mileage']
+                car_model_year = request.form['car_model_year']
+                car_horsepower = request.form['car_horsepower']
+                car_sold = request.form['car_sold']
+                car_date_added = now
+                car_price = request.form['car_price']
+                car_thumbnail = request.form['car_thumbnail']
+                car_image_2 = request.form['car_image_2']
+                car_image_3 = request.form['car_image_3']
+                car_image_4 = request.form['car_image_4']
+                car = cars(car_brand, car_model, car_sub_model, car_mileage, car_model_year, car_horsepower, car_sold, car_date_added, car_price, car_thumbnail, car_image_2, car_image_3, car_image_4)
+                db.session.add(car)
+                db.session.commit()
+                message = f"{car_brand} {car_model} {car_sub_model} er tilføjet til databasen."
+                return render_template('add_car.html', message=message)
+            else:
+                for field, errors in form1.errors.items():
+                    for error in errors:
+                        flash("Error in {}: {}".format(
+                            getattr(form1, field).label.text,
+                            error
+                        ), 'error')
+                return render_template('add_car.html', form1=form1, title='Tilføj ny bil')
+    else:
+        session['admin_login'] = 'not_admin'
+        return redirect("/")
 
 
 @app.route('/view_car', methods=['GET', 'POST'])
 def view_car():
-    if session['admin_login'] == 'not_admin':
-        return redirect('/')
+    if 'admin_login' in session:
+        if session['admin_login'] == 'not_admin':
+            return redirect('/')
+        else:
+            cars_view = cars.query.filter_by().order_by(cars.car_brand).all()
+            return render_template('view_car.html', cars_view=cars_view)
     else:
-        cars_view = cars.query.filter_by().order_by(cars.car_brand).all()
-        return render_template('view_car.html', cars_view=cars_view)
+        session['admin_login'] = 'not_admin'
+        return redirect("/")
 
 
 @app.route('/edit_or_delete', methods=['POST'])
 def edit_or_delete():
-    if session['admin_login'] == 'not_admin':
-        return redirect('/')
+    if 'admin_login' in session:
+        if session['admin_login'] == 'not_admin':
+            return redirect('/')
+        else:
+            id = request.form['id']
+            choice = request.form['choice']
+            car = cars.query.filter(cars.car_ID == id).first()
+            form1 = AddCar()
+            form2 = DeleteCar()
+            return render_template('edit_or_delete.html', car=car, form1=form1, form2=form2, choice=choice)
     else:
-        id = request.form['id']
-        choice = request.form['choice']
-        car = cars.query.filter(cars.car_ID == id).first()
-        form1 = AddCar()
-        form2 = DeleteCar()
-        return render_template('edit_or_delete.html', car=car, form1=form1, form2=form2, choice=choice)
+        session['admin_login'] = 'not_admin'
+        return redirect("/")
 
 
 @app.route('/delete_result', methods=['POST'])
 def delete_result():
-    if session['admin_login'] == 'not_admin':
-        return redirect('/')
-    else:
-        id = request.form['car_ID']
-        purpose = request.form['purpose']
-        car = cars.query.filter(cars.car_ID == id).first()
-        if purpose == 'delete':
-            db.session.delete(car)
-            db.session.commit()
-            message = f"Bilen {car.car_brand} {car.car_model} er blevet slettet fra databasen"
-            return render_template('result.html', message=message)
+    if 'admin_login' in session:
+        if session['admin_login'] == 'not_admin':
+            return redirect('/')
         else:
-            abort(405)
+            id = request.form['car_ID']
+            purpose = request.form['purpose']
+            car = cars.query.filter(cars.car_ID == id).first()
+            if purpose == 'delete':
+                db.session.delete(car)
+                db.session.commit()
+                message = f"Bilen {car.car_brand} {car.car_model} er blevet slettet fra databasen"
+                return render_template('result.html', message=message)
+            else:
+                abort(405)
+    else:
+        session['admin_login'] = 'not_admin'
+        return redirect("/")
 
 
 @app.route('/edit_result', methods=['POST'])
 def edit_result():
-    if session['admin_login'] == 'not_admin':
-        return redirect('/')
-    else:
-        id = request.form['car_ID']
-        car = cars.query.filter(cars.car_ID == id).first()
-        car.car_brand = request.form['car_brand']
-        car.car_model = request.form['car_model']
-        car.car_sub_model = request.form['car_sub_model']
-        car.car_mileage = request.form['car_mileage']
-        car.car_model_year = request.form['car_model_year']
-        car.car_horsepower = request.form['car_horsepower']
-        car.car_sold = request.form['car_sold']
-        car.car_price = request.form['car_price']
-        car.car_thumbnail = request.form['car_thumbnail']
-        car.car_image_2 = request.form['car_image_2']
-        car.car_image_3 = request.form['car_image_3']
-        car.car_image_4 = request.form['car_image_4']
-        form1 = AddCar()
-        if form1.validate_on_submit():
-            db.session.commit()
-            message = f"Bil med id: {car.car_ID} er blevet opdateret."
-            return render_template('result.html', message=message)
+    if 'admin_login' in session:
+        if session['admin_login'] == 'not_admin':
+            return redirect('/')
         else:
-            car.car_ID = id
-            for field, errors in form1.errors.items():
-                for error in errors:
-                    flash("Error in {}: {}".format(
-                        getattr(form1, field).label.text,
-                        error
-                    ), 'error')
-            return render_template('edit_or_delete.html', form1=form1, car=car, choice='edit')
+            id = request.form['car_ID']
+            car = cars.query.filter(cars.car_ID == id).first()
+            car.car_brand = request.form['car_brand']
+            car.car_model = request.form['car_model']
+            car.car_sub_model = request.form['car_sub_model']
+            car.car_mileage = request.form['car_mileage']
+            car.car_model_year = request.form['car_model_year']
+            car.car_horsepower = request.form['car_horsepower']
+            car.car_sold = request.form['car_sold']
+            car.car_price = request.form['car_price']
+            car.car_thumbnail = request.form['car_thumbnail']
+            car.car_image_2 = request.form['car_image_2']
+            car.car_image_3 = request.form['car_image_3']
+            car.car_image_4 = request.form['car_image_4']
+            form1 = AddCar()
+            if form1.validate_on_submit():
+                db.session.commit()
+                message = f"Bil med id: {car.car_ID} er blevet opdateret."
+                return render_template('result.html', message=message)
+            else:
+                car.car_ID = id
+                for field, errors in form1.errors.items():
+                    for error in errors:
+                        flash("Error in {}: {}".format(
+                            getattr(form1, field).label.text,
+                            error
+                        ), 'error')
+                return render_template('edit_or_delete.html', form1=form1, car=car, choice='edit')
+    else:
+        session['admin_login'] = 'not_admin'
+        return redirect("/")
 
 
 @app.route('/view_car_first', methods=['GET', 'POST'])
 def view_car_first():
-    if session['admin_login'] == 'not_admin':
-        return redirect('/')
+    if 'admin_login' in session:
+        if session['admin_login'] == 'not_admin':
+            return redirect('/')
+        else:
+            try:
+                cars_view = cars.query.filter_by().order_by(cars.car_brand).all()
+                car_text = '<ul>'
+                for car in cars_view:
+                    car_text += '<li>' + car.car_brand + ', ' + car.car_model + '</li>'
+                car_text += '</ul>'
+                return car_text
+            except Exception as e:
+                error_text = "<p>The error:<br>" + str(e) + "</p>"
+                hed = '<h1>Something is broken.</h1>'
+                return hed + error_text
     else:
-        try:
-            cars_view = cars.query.filter_by().order_by(cars.car_brand).all()
-            car_text = '<ul>'
-            for car in cars_view:
-                car_text += '<li>' + car.car_brand + ', ' + car.car_model + '</li>'
-            car_text += '</ul>'
-            return car_text
-        except Exception as e:
-            error_text = "<p>The error:<br>" + str(e) + "</p>"
-            hed = '<h1>Something is broken.</h1>'
-            return hed + error_text
+        session['admin_login'] = 'not_admin'
+        return redirect("/")
 
 
 @app.route('/testconnection')
 def testdb():
-    if session['admin_login'] == 'not_admin':
-        return redirect('/')
+    if 'admin_login' in session:
+        if session['admin_login'] == 'not_admin':
+            return redirect('/')
+        else:
+            try:
+                db.session.query(text('1')).from_statement(text('SELECT 1')).all()
+                return '<h1>It works.</h1>'
+            except Exception as e:
+                error_text = "<p>The error:<br>" + str(e) + "</p>"
+                hed = '<h1>Something is broken.</h1>'
+                return hed + error_text
     else:
-        try:
-            db.session.query(text('1')).from_statement(text('SELECT 1')).all()
-            return '<h1>It works.</h1>'
-        except Exception as e:
-            error_text = "<p>The error:<br>" + str(e) + "</p>"
-            hed = '<h1>Something is broken.</h1>'
-            return hed + error_text
+        session['admin_login'] = 'not_admin'
+        return redirect("/")
 
 
 @app.route("/login", methods=['POST', 'GET'])
 @mobile_template('{mobile/}login.html')
 def login(template):
-    if request.method == 'POST':
-        user = request.form["nm"]
-        session['admin_login'] = user
-        return redirect('/')
+    if 'admin_login' in session:
+        if request.method == 'POST':
+            user = request.form["nm"]
+            session['admin_login'] = user
+            return redirect('/')
+        else:
+            return render_template('login.html', title='Login')
     else:
-        return render_template('login.html', title='Login')
+        session['admin_login'] = 'not_admin'
+        return redirect("/")
 
 
 @app.route("/")
@@ -300,53 +332,81 @@ def index(template):
 @app.route("/biler")
 @mobile_template('{mobile/}biler.html')
 def biler(template):
-    info_table = dataconnection.get_info_table().fillna(-1)
-    table = dataconnection.get_newest_cars().fillna(-1)
-    table[['car_price']] = table[['car_price']].astype(int)
-    return render_template("biler.html", info_table=info_table, table=table, title='Biler på lager')
+    if 'admin_login' in session:
+        info_table = dataconnection.get_info_table().fillna(-1)
+        table = dataconnection.get_newest_cars().fillna(-1)
+        table[['car_price']] = table[['car_price']].astype(int)
+        return render_template("biler.html", info_table=info_table, table=table, title='Biler på lager')
+    else:
+        session['admin_login'] = 'not_admin'
+        return redirect("/")
 
 
 @app.route("/bil/<int:car_ID>")
 @mobile_template('{mobile/}bil.html')
 def bil(template, car_ID):
-    info_table = dataconnection.get_info_table().fillna(-1)
-    table = dataconnection.get_specific_car(car_ID).fillna(-1)
-    table[['car_price']] = table[['car_price']].astype(int)
-    return render_template("bil.html", info_table=info_table, table=table, title='Bil')
+    if 'admin_login' in session:
+        info_table = dataconnection.get_info_table().fillna(-1)
+        table = dataconnection.get_specific_car(car_ID).fillna(-1)
+        table[['car_price']] = table[['car_price']].astype(int)
+        return render_template("bil.html", info_table=info_table, table=table, title='Bil')
+    else:
+        session['admin_login'] = 'not_admin'
+        return redirect("/")
 
 
 @app.route("/kontakt_os")
 @mobile_template('{mobile/}kontakt_os.html')
 def kontakt_os(template):
-    info_table = dataconnection.get_info_table().fillna(-1)
-    return render_template("kontakt_os.html", info_table=info_table, title='Kontakt os')
+    if 'admin_login' in session:
+        info_table = dataconnection.get_info_table().fillna(-1)
+        return render_template("kontakt_os.html", info_table=info_table, title='Kontakt os')
+    else:
+        session['admin_login'] = 'not_admin'
+        return redirect("/")
 
 
 @app.route("/om_os")
 @mobile_template('{mobile/}om_os.html')
 def om_os(template):
-    info_table = dataconnection.get_info_table().fillna(-1)
-    return render_template("om_os.html", info_table=info_table, title='Om os')
+    if 'admin_login' in session:
+        info_table = dataconnection.get_info_table().fillna(-1)
+        return render_template("om_os.html", info_table=info_table, title='Om os')
+    else:
+        session['admin_login'] = 'not_admin'
+        return redirect("/")
 
     
 @app.route("/Admin")
 @mobile_template('{mobile/}admin_ui.html')
 def Admin(template):
-    info_table = dataconnection.get_info_table().fillna(-1)
-    return render_template("admin_ui.html", info_table=info_table, title='Admin')
+    if 'admin_login' in session:
+        info_table = dataconnection.get_info_table().fillna(-1)
+        return render_template("admin_ui.html", info_table=info_table, title='Admin')
+    else:
+        session['admin_login'] = 'not_admin'
+        return redirect("/")
 
 
 @app.route("/qrtest")
 @mobile_template('{mobile/}qrtest.html')
 def qrtest(template):
-    info_table = dataconnection.get_info_table().fillna(-1)
-    newest_car = dataconnection.get_newest_car().fillna(-1)
-    newest_car[['car_price']] = newest_car[['car_price']].astype(int)
-    return render_template("qrtest.html", info_table=info_table, newest_car=newest_car, title='Qrtest')
+    if 'admin_login' in session:
+        info_table = dataconnection.get_info_table().fillna(-1)
+        newest_car = dataconnection.get_newest_car().fillna(-1)
+        newest_car[['car_price']] = newest_car[['car_price']].astype(int)
+        return render_template("qrtest.html", info_table=info_table, newest_car=newest_car, title='Qrtest')
+    else:
+        session['admin_login'] = 'not_admin'
+        return redirect("/")
 
 
 @app.route("/form")
 @mobile_template('{mobile/}form.html')
 def form(template):
-    info_table = dataconnection.get_info_table().fillna(-1)
-    return render_template("form.html", info_table=info_table, title='Form')
+    if 'admin_login' in session:
+        info_table = dataconnection.get_info_table().fillna(-1)
+        return render_template("form.html", info_table=info_table, title='Form')
+    else:
+        session['admin_login'] = 'not_admin'
+        return redirect("/")
